2a5d2f8be33ef6305bac658267c681d858e1b7e0
Ezhil089 <ezhil.arasan62@gmail.com>
2020-01-08 20:21:36 +0530

    added HelloWorld program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,24 +1,10 @@
 import java.io.IOException;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 public class HelloWorld 
 {
 	public static void main(String args[]) throws InterruptedException, IOException
 	{
 		System.out.println("Welcome to helloworld program");
-//	    System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
-//	    WebDriver driver=new ChromeDriver();	
-//	    driver.get("http://demo.guru99.com/test/upload/");			
-//
-//	    
-//	    driver.findElement(By.name("uploadfile_0")).click();
-//	    Thread.sleep(4000);
-//	     Runtime.getRuntime().exec("C:\\d drive\\Auto IT\\FileUpload.exe");	
-//	     Thread.sleep(2000);
-//	     driver.close();
 	     System.out.println("Closed successful");
 	}
 }
