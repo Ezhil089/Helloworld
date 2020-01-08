b187e1c106c915bb8b0099d0368199307de9ae06
Ezhil089 <ezhil.arasan62@gmail.com>
2020-01-08 19:59:04 +0530

    added HelloWorld program

--- HelloWorld.java
+++ HelloWorld.java
@@ -9,16 +9,16 @@ public class HelloWorld
 	public static void main(String args[]) throws InterruptedException, IOException
 	{
 		System.out.println("Welcome to helloworld program");
-	    System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
-	    WebDriver driver=new ChromeDriver();	
-	    driver.get("http://demo.guru99.com/test/upload/");			
-
-	    
-	    driver.findElement(By.name("uploadfile_0")).click();
-	    Thread.sleep(4000);
-	     Runtime.getRuntime().exec("C:\\d drive\\Auto IT\\FileUpload.exe");	
-	     Thread.sleep(2000);
-	     driver.close();
+//	    System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+//	    WebDriver driver=new ChromeDriver();	
+//	    driver.get("http://demo.guru99.com/test/upload/");			
+//
+//	    
+//	    driver.findElement(By.name("uploadfile_0")).click();
+//	    Thread.sleep(4000);
+//	     Runtime.getRuntime().exec("C:\\d drive\\Auto IT\\FileUpload.exe");	
+//	     Thread.sleep(2000);
+//	     driver.close();
 	     System.out.println("Closed successful");
 	}
 }
